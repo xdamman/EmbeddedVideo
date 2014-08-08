5124b368b3a692044cdc9b2fab4f32bcb28b4e48
Xavier Damman <xdamman@gmail.com>
2014-08-08 14:54:24 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,92 @@
+var twitter = require('twitter')
+  , env = process.env.NODE_ENV || "development"
+  , request = require('request')
+  , utils = require('./lib/utils')
+  , settings = require('./settings.'+env+'.json');
+
+var twit = new twitter(settings.twitter);
+
+
+var sendTweetReply = function(in_reply_to_status_id, text, cb) {
+    var form, r;
+    var oauthKeys = {
+      consumer_key: settings.twitter.consumer_key,
+      consumer_secret: settings.twitter.consumer_secret,
+      token: settings.twitter.access_token_key,
+      token_secret: settings.twitter.access_token_secret
+    };
+    r = request.post("https://api.twitter.com/1.1/statuses/update.json", {oauth: oauthKeys}, cb);
+    form = r.form();
+    form.append('status', text);
+    form.append('in_reply_to_status_id', in_reply_to_status_id);
+    r.on('error', function(e) {
+      console.error("Error while sending the tweet: ", e);
+    });
+  }
+
+function processTweet(tweet) {
+
+  if(!tweet.entities) return;
+  if(!tweet.entities.urls || tweet.entities.urls.length == 0) return;
+  if(tweet.user.screen_name == 'EmbeddedVideo') { 
+    console.log("tweet from EmbeddedVideo: ", tweet);
+    return;
+  }
+
+  var url = tweet.entities.urls[0].expanded_url;
+
+  console.log("Getting HTML page for ", url);
+  utils.getHTML(url, function(e, html) {
+    var videoUrl = utils.getVideoUrl(html);
+
+    if(!videoUrl) {
+      console.log("No video found in "+url);
+      return;
+    }
+
+    utils.getHTML(videoUrl, function(e, html) {
+      var title = utils.getTitle(html);
+
+      title = title.replace(" - YouTube","");
+      title = title.replace(" on Vimeo","");
+
+      var via = "@"+tweet.user.screen_name;
+      var text = title.smart_truncate(119-via.length);
+
+      text = via + " " +text + " " + videoUrl
+
+      console.log("Sending tweet to ", tweet.id_str, text);
+      sendTweetReply(tweet.id_str, text, function(err, result) {
+        console.log("Tweet sent", err);
+      });
+    });
+  });
+
+};
+
+twit.stream('user', function(stream) {
+
+  stream.on('data', function(data) {
+
+    var tweet = data.retweeted_status || data;
+
+    console.log("\n-----------------------------------------------------------------------------\n");
+
+    if(tweet.user) {
+      console.log("Tweet from : "+tweet.user.screen_name, tweet.text);
+      processTweet(tweet);
+    }
+
+  });
+
+  stream.on('error', function(e) {
+    console.error("Error in twitter stream", e);
+    process.exit(1);
+  });
+
+  stream.on('end', function(e) {
+    console.log("Twitter stream ended -- exiting");
+    process.exit(1);
+  });
+
+});
---
+++ test/utils.spec.js
@@ -0,0 +1,50 @@
+var utils = require('../lib/utils');
+var expect = require('chai').expect;
+
+var url = "http://www.upworthy.com/to-help-the-worlds-kids-stay-healthy-people-got-thinky-and-what-they-came-up-with-is-super-smart";
+var html;
+
+describe('utils', function() {
+
+  it('downloads the html of a page', function(done) {
+    utils.getHTML(url, function(e, content) {
+      expect(e).to.not.exist;
+      html = content;
+      expect(html.length > 20000).to.be.true;
+      done();
+    });
+  });
+
+  it('gets the title from the html', function() {
+    var title = utils.getTitle(html);
+    expect(title).to.equal('To Help The World’s Kids Stay Healthy, People Got Thinky, And What They Came Up With Is Super-Smart');
+  });
+
+});
+
+describe("Vimeo", function() {
+  it('gets the video url from the html', function() {
+    var videoUrl = utils.getVideoUrl(html);
+    expect(videoUrl).to.equal("https://vimeo.com/96580414");
+  });
+});
+
+describe("YouTUBE", function() {
+  it('gets the video url from the html', function(done) {
+    utils.getHTML("http://www.theverge.com/2014/8/7/5977917/ronaldo-puts-his-face-behind-absurd-japanese-smile-enhancer", function(e, html) {
+      var videoUrl = utils.getVideoUrl(html);
+      expect(videoUrl).to.equal("https://youtube.com/watch?v=1c4O6FlEktc");
+      done();
+    });
+  });
+
+  it('gets the video url from the html when there is no ?params', function(done) {
+    utils.getHTML("http://huff.to/1sldfLI", function(e, html) {
+      var videoUrl = utils.getVideoUrl(html);
+      expect(videoUrl).to.equal("https://youtube.com/watch?v=-e-VKjl72z4");
+      done();
+    });
+  });
+
+
+});
